45cbf2f8b16aad51ef57aef3fe57e9b43e4ea44e
Shivam Kushwaha <shivamk8368@gmail.com>
2022-04-01 12:25:32 -0500

    added database for homepage

--- webpages/admin.py
+++ webpages/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Latest_work, Team
 # Register your models here.
+
+
+admin.site.register(Latest_work)
+admin.site.register(Team)
--- webpages/models.py
+++ webpages/models.py
@@ -1,10 +1,32 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
 
 class Latest_work(models.Model):
-    photo = models.ImageField()
+    photo = models.ImageField(upload_to = 'media/latest_work/%Y/%m/')
+    photo_desc = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
     
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.first_name
+        return self.photo_desc
+
+
+
+class Team(models.Model):
+
+    team_photo = models.ImageField(upload_to = 'media/team/%Y/%m/')
+    team_name = models.CharField(max_length=255)
+    team_position = models.CharField(max_length=255)
+    team_email = models.EmailField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.team_name
+
